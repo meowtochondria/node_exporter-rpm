3b8263324845b44f0de3e9af042a75dc82e30b7a
LamGC <lam827@lamgc.net>
2025-12-16 11:27:24 +0800

    fix: Remove hardcoded product_arch in spec and restore BuildArch.

--- build.sh
+++ build.sh
@@ -314,4 +314,4 @@ download_packages
 # _topdir and _tmppath are magic rpm variables that can be defined in ~/.rpmmacros
 # For ease of reliable builds they are defined here on the command line.
 print_debug_line "Starting rpmbuild."
-rpmbuild -ba --define="_topdir $build_root" --define="buildroot $build_root/BUILDROOT" --define="pkg_version $pkg_version" --define="rpm_release $pkg_release" --target "$rpm_arch" $build_root/SPECS/$product.spec
+rpmbuild -ba --define="_topdir $build_root" --define="buildroot $build_root/BUILDROOT" --define="pkg_version $pkg_version" --define="rpm_release $pkg_release" --define="product_arch $product_arch" --define="build_arch $rpm_arch" --target "$rpm_arch" $build_root/SPECS/$product.spec
